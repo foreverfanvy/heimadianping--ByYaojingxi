2088f213b6f9a28dc471cbac1b1cfe0ac725daeb
晨曦中的暮雨 <3075514079@qq.com>
2025-08-06 15:21:01 +0800

    14.利用逻辑判断解决了部分误删逻辑，但是在获取锁id判断是否相同和释放锁直接可能存在的误删问题没有进行解决

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 
@@ -9,6 +10,7 @@ public class SimpleRedisLock implements ILock {
     private String name;
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "";//使用UUID生成一个唯一的ID前缀，避免不同锁之间的冲突
 
     //进行构造人工注入
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
@@ -21,15 +23,23 @@ public class SimpleRedisLock implements ILock {
         //value有线程的标识
         //获取当前线程的唯一标识
         long ThreadID = Thread.currentThread().getId();
-        String value = ThreadID + "";
-        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, value, timeoutSec, TimeUnit.SECONDS);
+        String value = ID_PREFIX + ThreadID + "";
+        String key = KEY_PREFIX + name;
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(key, value, timeoutSec, TimeUnit.SECONDS);
         //这里如果直接返回lock会有一个自动拆箱的过程，可能会导致空指针异常
         return lock != null && lock; //如果获取锁成功，返回true
     }
 
     @Override
     public void unLock() {
-        //释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        //获取线程标识
+        String ThreadID = ID_PREFIX + Thread.currentThread().getId() + "";
+        //获取锁的唯一标识
+        String lockId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if (ThreadID.equals(lockId)) {
+            //释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
+
     }
 }
